e0825d2ba5f1d9c614996ce2612fe14a45ecab8f
unknown <g.diaby3@gmail.com>
2026-06-28 18:56:32 +0200

    etape 3 : réagir au clic

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 const form = document.querySelector("#invocation")
+const prenom = document.querySelector("#prenom").value
+const creature = document.querySelector("#creature").value 
+const message = document.querySelector("#message")
+
+
+
+form.addEventListener('click', () => {
+    event.preventDefault()
+    message.textContent = `${prenom} a invoqué ${creature} pour la suivre dans son aventure`
+    const formCache = document.querySelector("#invocation").classList.add('cache')
+    const resultat = document.querySelector("#resultat").classList.remove('cache')
+    console.log('clic')
+})
+
