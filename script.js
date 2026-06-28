c6371c4f7507011706ed8442fd5c45d056f1dad8
unknown <g.diaby3@gmail.com>
2026-06-28 19:43:05 +0200

    etape 4 : ecran creature

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const form = document.querySelector("#invocation")
 const prenom = document.querySelector("#prenom").value
-const creature = document.querySelector("#creature").value 
-const message = document.querySelector("#message")
+const btnRecommencer = document.querySelector("#recommencer").value
+
 
 
 
 form.addEventListener('click', () => {
     event.preventDefault()
-    message.textContent = `${prenom} a invoqué ${creature} pour la suivre dans son aventure`
-    const formCache = document.querySelector("#invocation").classList.add('cache')
+    const creature = document.querySelector("#creature").value 
+    const message = document.querySelector("#message")
+    const form = document.querySelector("#invocation").classList.add('cache')
     const resultat = document.querySelector("#resultat").classList.remove('cache')
+    message.textContent = `${prenom} a invoqué ${creature} pour la suivre dans son aventure`
     console.log('clic')
 })
 
+btnRecommencer.addEventListener('click', () =>{
+    event.preventDefault()
+
+
+})
